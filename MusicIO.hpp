04d7834e286bf827ff2018adc2d466a34e3d2a43
Wen-Yi Hsiao <s101062219@gmail.com>
2020-10-05 11:55:17 +0800

    Create MusicIO.hpp

---
+++ MusicIO.hpp
@@ -0,0 +1,39 @@
+//
+//  MusicIO.hpp
+//  console_renderer - ConsoleApp
+//
+//  Created by Wen-Yi hsiao on 2020/10/5.
+//
+
+#ifndef MusicIO_hpp
+#define MusicIO_hpp
+
+#include <JuceHeader.h>
+#include <stdio.h>
+
+using namespace juce;
+
+
+namespace MusicIO {
+
+
+struct AudioFileInfo{
+    int sampleRate;
+    int bitsPerSample;
+    int sampleLength;
+    int numChannels;
+};
+
+
+AudioFileInfo readWavFile(String pathToAudioInFile, AudioBuffer<float>& inBuffer);
+void writeWavFile(
+        String pathToAudioOutFile,
+        AudioBuffer<float>& outBuffer,
+        int sampleRate,
+        int bitsPerSample);
+void readMidiFile(String pathToAudioInFile, int sampleRate, MidiBuffer& midiBuffer);
+
+} // namespace MusicIO
+
+
+#endif /* MusicIO_hpp */
